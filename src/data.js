952fc690f2c5768bd736fe40bfda69cbf7eb5496
Chahat Bhatia <artisticoder@Chahats-Mac.local>
2023-11-22 18:21:46 +0530

    feature/carousel-component

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const images = [
+   "https://wowslider.com/sliders/demo-77/data1/images/tuscany428041.jpg",
+   "https://wowslider.com/sliders/demo-77/data1/images/road220058.jpg",
+   "https://wowslider.com/sliders/demo-77/data1/images/field175959_1920.jpg",
+   "https://wowslider.com/sliders/demo-77/data1/images/idaho239691_1920.jpg"
+]
