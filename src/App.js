1c6e7d131a4473da822561b2f1665acba61a4853
Saksham <saksham.kumar@squadiq.in>
2023-12-29 15:25:12 +0530

    added code for polling

--- src/App.js
+++ src/App.js
@@ -1,98 +1,121 @@
-import { useEffect, useRef, useState } from "react";
-import "./App.css";
+import React, { useMemo, useState } from "react";
 
-const rowConfig = [
-  [true, true, true],
-  [true, false, false],
-  [true, true, true],
+const pollingItems = [
+  {
+    key: "Javascript",
+    name: "Javascript",
+    isChecked: false,
+    voteCount: 25,
+  },
+  {
+    key: "html",
+    name: "HTML",
+    isChecked: false,
+    voteCount: 25,
+  },
+  {
+    key: "CSS",
+    name: "CSS",
+    isChecked: false,
+    voteCount: 24,
+  },
+  {
+    key: "React",
+    name: "React",
+    isChecked: false,
+    voteCount: 25,
+  },
 ];
 
-function App() {
-  const [config, setConfig] = useState([]);
-  const [isResetting, setIsResetting] = useState(false);
-  const [mapper, setMapper] = useState([]);
+const App = () => {
+  const [pollData, setPollDataState] = useState(pollingItems);
+  const [isShowPollPercentage, setShowPollPecentage] = useState(false);
 
-  const countRef = useRef();
-
-  const onReset = (updatedMapper) => {
-    for (let i = 0; i < updatedMapper.length; i++) {
-      setIsResetting(true);
-      const interval = setTimeout(() => {
-        const transformedState = [...config];
-        const [parentIndex, childIndex] = updatedMapper[i].split("_");
-        transformedState[parseInt(parentIndex)][parseInt(childIndex)].color =
-          "white";
-        setConfig(transformedState);
-        if (i === updatedMapper.length - 1) {
-          clearInterval(interval);
-          setMapper([]);
-          setIsResetting(false);
-        }
-      }, 1000 * i);
-    }
-  };
-
-  useEffect(() => {
-    countRef.current = 0;
-    const transformedState = rowConfig.map((item, parentIndex) => {
-      return item.map((box, childIndex) => {
-        countRef.current = box ? countRef.current + 1 : countRef.current;
+  const onInputSelected = (index) => {
+    setPollDataState((prev) => {
+      const prevCopy = prev.map((item, i) => {
         return {
-          isSelected: box,
-          boxIdentifier: `${parentIndex}_${childIndex}`,
-          color: "white",
+          ...item,
+          isChecked: i === index,
         };
       });
+
+      return prevCopy;
     });
-    setConfig(transformedState);
-  }, []);
-
-  console.log(countRef);
-
-  const onItemClicked = (parentIndex, childIndex) => {
-    if (isResetting) {
-      return;
-    }
-    const copyConfig = [...config];
-    const boxIndex = `${parentIndex}_${childIndex}`;
-    if (copyConfig[parentIndex][childIndex].color === "white") {
-      copyConfig[parentIndex][childIndex].color = "green";
-      const updatedMapper = [...mapper, `${boxIndex}`];
-      setMapper(updatedMapper);
-      updatedMapper.length === countRef.current && onReset(updatedMapper);
-    } else {
-      copyConfig[parentIndex][childIndex].color = "white";
-      setMapper((prev) => {
-        return prev.map((item) => item !== boxIndex);
-      });
-    }
-
-    setConfig(copyConfig);
+    setShowPollPecentage(true);
   };
 
+  const totalVotes = useMemo(() => {
+    let totalVotesCount = 0;
+    pollData.map(({ voteCount, isChecked }) => {
+      totalVotesCount += voteCount;
+      if (isChecked) {
+        totalVotesCount += 1;
+      }
+    });
+    return totalVotesCount;
+  }, [pollData]);
+
   return (
-    <div className="App">
-      {(config || []).map((item, parentIndex) => {
-        const rowItems = item.map(
-          ({ isSelected, boxIdentifier, color }, childIndex) => {
-            return isSelected ? (
-              <div
-                key={boxIdentifier}
-                onClick={() => onItemClicked(parentIndex, childIndex)}
-                style={{
-                  height: "100px",
-                  width: "100px",
-                  border: "1px solid black",
-                  background: color,
-                }}
-              ></div>
-            ) : null;
-          }
+    <div
+      style={{
+        margin: "20px",
+        padding: "20px",
+        border: "1px solid red",
+        width: "25%",
+      }}
+    >
+      <p style={{ marginTop: "10px" }}> Select your most strong skill</p>
+      {pollData.map(({ key, name, isChecked, voteCount }, index) => {
+        const currentVoteCount = isChecked ? voteCount + 1 : voteCount;
+        const percentage = (currentVoteCount / totalVotes) * 100;
+        return (
+          <div
+            key={key}
+            style={{
+              marginBottom: "20px",
+              border: isShowPollPercentage ? "1px solid green" : null,
+
+              padding: "10px",
+            }}
+          >
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={() => onInputSelected(index)}
+              />
+              <div style={{ marginRight: "10px" }}>{name}</div>
+            </div>
+            {isShowPollPercentage && (
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <div
+                  style={{
+                    width: "80%",
+                    backgroundColor: "#ddd",
+                    borderRadius: "5px",
+                    height: "10px",
+                  }}
+                >
+                  <div
+                    style={{
+                      width: `${isChecked ? percentage : percentage}%`,
+                      backgroundColor: "#4caf50",
+                      borderRadius: "5px",
+                      height: "100%",
+                    }}
+                  />
+                </div>
+                <div style={{ marginLeft: "10px" }}>
+                  {percentage.toFixed(2)}%
+                </div>
+              </div>
+            )}
+          </div>
         );
-        return <div style={{ display: "flex" }}>{rowItems}</div>;
       })}
     </div>
   );
-}
+};
 
 export default App;
