4844547e978887991da1672f5528d845eb2b33b0
Saksham <saksham.kumar@squadiq.in>
2024-01-17 00:56:25 +0530

    added scrolling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+
+var my_array = [];
+
+for (var i = 1; i <= 1000; i++) {
+  my_array.push(i);
+}
+
+const PAGE_SIZE = 10;
+const START_PAGE = 0;
 
 function App() {
+  const [data, setData] = useState(my_array.slice(START_PAGE, PAGE_SIZE));
+  const pageRef = useRef(START_PAGE); // Use ref to keep track of the page
+  const bottom = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        pageRef.current += 1; // Update the ref value
+        setData((prevData) => [
+          ...prevData,
+          ...my_array.slice(
+            pageRef.current * PAGE_SIZE,
+            (pageRef.current + 1) * PAGE_SIZE
+          ),
+        ]);
+      }
+    });
+    observer.observe(bottom.current);
+
+    // Cleanup the observer on component unmount
+    return () => observer.disconnect();
+  }, []); // Empty dependency array to run the effect only once
+
+  console.log("Page Outside: ", pageRef.current);
+  console.log("Data Outside: ", data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {data.map((item) => (
+        <div
+          key={item}
+          style={{
+            height: "70px",
+            width: "50px",
+            background: "yellow",
+            marginTop: "10px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {item}
+        </div>
+      ))}
+      <div
+        ref={bottom}
+        style={{ height: "2px", background: "blue", width: "50px" }}
+      />
     </div>
   );
 }
