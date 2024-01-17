b69965e507e1d158b8dca231e3c90bddfab8a67d
Saksham <saksham.kumar@squadiq.in>
2024-01-17 13:55:37 +0530

    added stopwatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const timer = useRef(null);
+
+  const onStartClick = () => {
+    timer.current = setInterval(() => {
+      setCounter((prev) => prev + 1);
+    }, 1000);
+  };
+
+  const onStopClicked = () => {
+    clearInterval(timer.current);
+    setCounter(0);
+  };
+
+  const onPauseClicked = () => {
+    clearInterval(timer.current);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{counter}</div>
+      <button onClick={onStartClick}>start</button>
+      <button onClick={onStopClicked}>stop</button>
+      <button onClick={onPauseClicked}>pause</button>
     </div>
   );
 }
